c4baa61e0fd721f7c5d36804348346b95c9e92a5
x <dheandaallen@gmail.com>
2022-07-22 14:28:24 +0700

    Initial Commit

---
+++ Nano Challenge 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nano Challenge 2
+//
+//  Created by Dheanda Allen on 22/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nano Challenge 2/Nano_Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_Challenge_2App.swift
+//  Nano Challenge 2
+//
+//  Created by Dheanda Allen on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Nano_Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
