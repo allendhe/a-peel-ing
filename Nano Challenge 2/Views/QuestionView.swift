35a16c54ad3629c8d0d8fd7f17802ddfce2a9b69
x <dheandaallen@gmail.com>
2022-07-27 18:45:02 +0700

    question view and lottie working

--- Nano Challenge 2/Views/QuestionView.swift
+++ Nano Challenge 2/Views/QuestionView.swift
@@ -8,39 +8,65 @@
 import SwiftUI
 
 struct QuestionView: View {
-  @State var QuestionIndex = 0
-//    @State private var orientation
-//    = UIDeviceOrientation.unknown
+    @State var QuestionIndex = 0 {
+        didSet{
+            if QuestionIndex > QuestionData.shared.count - 1 {
+                QuestionIndex = 0
+            }
+        }
+    }
+    
+    @State var isAnswered = false
+    
+    func buttonClick(){
+        QuestionIndex = QuestionIndex + 1
+        isAnswered.toggle()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            isAnswered.toggle()
+                }
+    }
     
     var body: some View {
-        VStack(spacing:50){
-            HStack{
-                Text(QuestionData.shared[QuestionIndex].QuestionString)
-                    .question()
-        
-            }
-            Image(QuestionData.shared[QuestionIndex].QuestionImage)
-                .padding(.bottom,40)
+        ZStack {
+            VStack(spacing:50){
+                HStack{
+                    Text(QuestionData.shared[QuestionIndex].QuestionString)
+                        .question()
             
-            HStack{
-                OptionButton(text: QuestionData.shared[QuestionIndex].QuestionOption[0])
-                    .padding().onTapGesture {
-                        QuestionIndex = QuestionIndex + 1
-                    }
-                OptionButton(text: QuestionData.shared[QuestionIndex].QuestionOption[1])
-                    .padding().onTapGesture {
-                        QuestionIndex = QuestionIndex + 1
-                    }
-            }
-           
+                }
+                Image(QuestionData.shared[QuestionIndex].QuestionImage)
+                    .padding(.bottom,40)
+                
+                HStack{
+                    OptionButton(text: QuestionData.shared[QuestionIndex].QuestionOption[0])
+                        .padding().onTapGesture {
+                            buttonClick()
+                        }
+                    OptionButton(text: QuestionData.shared[QuestionIndex].QuestionOption[1])
+                        .padding().onTapGesture {
+                            buttonClick()
+                        }
+                }
+        }
             
-    }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background{Color(red: 1, green: 0.9922, blue: 0.9608)}
-     
+            
+            if isAnswered{
+                
+             VStack {
+                LottieView(fileName: "confetti")
+                    .frame(width: 800, height: 400, alignment: .center)
+                
+                Text("You are correct")
+                
+             }
+            }
+        }
 }
-
+   
+    
 struct QuestionView_Previews: PreviewProvider {
     static var previews: some View {
         QuestionView()
